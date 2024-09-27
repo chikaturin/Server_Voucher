e3d1440669edd979d736dc29585261fd00615b62
Chikaturin <ninhhoaithanh@gmail.com>
2024-09-27 13:43:58 +0700

    "Rename '/deleteVoucher/:_id' to '/DeleteVoucher/:_id' in VoucherRouter.js"

--- Router/VoucherRouter.js
+++ Router/VoucherRouter.js
@@ -27,6 +27,6 @@ router.get(
 
 router.put("/updateVoucher/:_id", updateVoucher);
 router.post("/updateState/:_id", updateState);
-router.get("/deleteVoucher/:_id", deleteVoucher);
+router.get("/DeleteVoucher/:_id", deleteVoucher);
 
 module.exports = router;
