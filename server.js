b14e982edc3c55ebc72f85401128ecf8a96da981
Chikaturin <ninhhoaithanh@gmail.com>
2024-10-28 09:51:10 +0700

    thanh remove cmt

--- server.js
+++ server.js
@@ -27,26 +27,26 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// const kafka = new Kafka({
-//   clientId: "my-consumer",
-//   brokers: ["localhost:9092"],
-// });
+const kafka = new Kafka({
+  clientId: "my-consumer",
+  brokers: ["localhost:9092"],
+});
 
-// const consumer = kafka.consumer({ groupId: "my-group" });
+const consumer = kafka.consumer({ groupId: "my-group" });
 
-// const run = async () => {
-//   await consumer.connect();
-//   await consumer.subscribe({ topic: "useVoucher", fromBeginning: true });
+const run = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: "useVoucher", fromBeginning: true });
 
-//   await consumer.run({
-//     eachMessage: async ({ topic, partition, message }) => {
-//       console.log({
-//         partition,
-//         offset: message.offset,
-//         value: message.value.toString(),
-//       });
-//     },
-//   });
-// };
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      });
+    },
+  });
+};
 
-// run().catch(console.error);
+run().catch(console.error);
