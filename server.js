4885aeedcc80985c7c319e7559e855202cf7f7e4
Chikaturin <ninhhoaithanh@gmail.com>
2024-11-04 15:30:46 +0700

    thanh 14:52

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { Kafka } = require("kafkajs");
 
 app.use(cors());
 app.use(express.json());
-const PORT = 3002;
+const PORT = 3000;
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
