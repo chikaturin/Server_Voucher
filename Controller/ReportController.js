062cdf7159c2cad6c55bed75ac26b872bc31388b
Chikaturin <ninhhoaithanh@gmail.com>
2024-11-14 03:10:51 +0700

    thanh 20:30

--- Controller/ReportController.js
+++ Controller/ReportController.js
@@ -89,6 +89,7 @@ const SolveReport = async (req, res) => {
     if (!report) {
       return res.status(404).json({ message: "Report not found" });
     }
+    numredis = math.floor(Math.random() * 100);
     res.status(200).json({ message: "Solve Report successfully" });
   } catch (error) {
     res.status(400).json({ message: error.message });
