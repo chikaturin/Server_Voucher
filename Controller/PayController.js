f4b0803950556c7fe88f5084422c5cbc176abefb
Chikaturin <ninhhoaithanh@gmail.com>
2024-11-27 12:10:04 +0700

    "Updated PX value in redisClient.set from 1000 to 3000 in ApplyVoucher function"

--- Controller/PayController.js
+++ Controller/PayController.js
@@ -348,7 +348,7 @@ const ApplyVoucher = async (req, res) => {
 
     const lockAcquired = await redisClient.set(lockKey, "locked", {
       NX: true,
-      PX: 1000,
+      PX: 3000,
     });
 
     if (!lockAcquired) {
